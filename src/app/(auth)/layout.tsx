45391d5863ae389bfcc066d275aa87e263cd6cdf
Aman Kumar Gupta <akgbytes@gmail.com>
2025-10-06 19:15:50 +0530

    Added clerk for authentication

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from "react";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div className="min-h-svh flex items-center justify-center">{children}</div>
+  );
+}
--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -1,15 +1,27 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { UserCircleIcon } from "lucide-react";
 
+import { UserButton, SignedOut, SignedIn, SignInButton } from "@clerk/nextjs";
+
 const AuthButton = () => {
   return (
-    <Button
-      variant="outline"
-      className="px-4 py-2 text-sm font-medium rounded-full shadow-none text-blue-600 hover:text-blue-500 border-blue-500/20"
-    >
-      <UserCircleIcon />
-      Sign in
-    </Button>
+    <>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button
+            variant="outline"
+            className="px-4 py-2 text-sm font-medium rounded-full shadow-none text-blue-600 hover:text-blue-500 border-blue-500/20"
+          >
+            <UserCircleIcon />
+            Sign in
+          </Button>
+        </SignInButton>
+      </SignedOut>
+    </>
   );
 };
 
