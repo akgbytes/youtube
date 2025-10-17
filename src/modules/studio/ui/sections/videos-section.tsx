8933459ac5903db6084cff72d7922a6a32e0b519
Aman Kumar Gupta <akgbytes@gmail.com>
2025-10-17 23:07:52 +0530

    Implement infinite scroll

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -1,12 +1,36 @@
 "use client";
 
 import { DEFAULT_LIMIT } from "@/app/constants";
+import { InfiniteScroll } from "@/components/infinite-scroll";
 import { trpc } from "@/trpc/client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import Link from "next/link";
 
 interface VideosSectionProps {}
 
-const VideosSection = ({}: VideosSectionProps) => {
-  const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery(
+const VideosSection = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ErrorBoundary fallback={<div>Error</div>}>
+        <VideosSectionSuspense />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+const VideosSectionSuspense = ({}: VideosSectionProps) => {
+  const [videos, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
     {
       limit: DEFAULT_LIMIT,
     },
@@ -15,7 +39,54 @@ const VideosSection = ({}: VideosSectionProps) => {
     }
   );
 
-  return <div>{JSON.stringify(data)}</div>;
+  return (
+    <div>
+      <div className="border-y">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="pl-6 w-[510px]">Video</TableHead>
+              <TableHead>Visibility</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead>Views</TableHead>
+              <TableHead>Comments</TableHead>
+              <TableHead>Likes</TableHead>
+            </TableRow>
+          </TableHeader>
+
+          <TableBody>
+            {videos.pages
+              .flatMap((page) => page.videos)
+              .map((video) => (
+                <Link
+                  href={`/studio/videos/${video.id}`}
+                  key={video.id}
+                  legacyBehavior
+                >
+                  <TableRow className="cursor-pointer">
+                    <TableCell>{video.title}</TableCell>
+                    <TableCell>Visibility</TableCell>
+                    <TableCell>status</TableCell>
+                    <TableCell>date</TableCell>
+                    <TableCell>views</TableCell>
+                    <TableCell>comments</TableCell>
+                    <TableCell>likes</TableCell>
+                  </TableRow>
+                </Link>
+              ))}
+          </TableBody>
+        </Table>
+      </div>
+
+      <InfiniteScroll
+        isManual
+        fetchNextPage={query.fetchNextPage}
+        hasNextPage={query.hasNextPage}
+        isFetchingNextPage={query.isFetchingNextPage}
+      />
+    </div>
+  );
 };
 
 export default VideosSection;
